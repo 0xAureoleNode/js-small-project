54534ac9702440faf8928b529b86b966e0071ccf
”0xAureoleNode“ <aureolenode@gmail.com>
2023-01-01 19:39:50 +0800

    project 2 init

--- Project1-Modal/script.js
+++ Project1-Modal/script.js
@@ -1,11 +1,10 @@
 // get the open btn
-/*
+
 const openBtn = document.querySelector('.open');
 // get the modal-container
 const container = document.querySelector('.modal-container');
 // get the close btn
 const closeBtn = document.querySelector('.modal-btn');
-
 
 // event
 openBtn.addEventListener('click', function () {
@@ -13,13 +12,4 @@ openBtn.addEventListener('click', function () {
 });
 closeBtn.addEventListener('click', function () {
   container.classList.remove('show');
-});
-*/
-
-// 能不能用另外的方法实现？toggle
-const btn = document.querySelector('.btn');
-const container = document.querySelector('.modal-container');
-
-btn.addEventListener('click', function () {
-  container.classList.toggle('show');
 });
